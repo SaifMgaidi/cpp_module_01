549a87cca6ab59c249caab579d7ff4b83e4417a3
Saif <saif4547@gmail.com>
2026-02-11 23:39:29 +0100

    ex02 finish

---
+++ ex02/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+int	main(void)
+{
+	std::string	str = "HI THIS IS BRAIN";
+	std::string	*stringPTR = &str;
+	std::string	&stringREF = str;
+
+	std::cout << "memory address of string: " << &str << std::endl;
+	std::cout << "memory address of stringPTR: " << stringPTR << std::endl;
+	std::cout << "memory address of stringREF: " << &stringREF << std::endl;
+	std::cout << "value of string: " << str << std::endl;
+	std::cout << "value of stringPTR: " << *stringPTR << std::endl;
+	std::cout << "value of stringREF: " << stringREF << std::endl;
+
+	return (0);
+}
